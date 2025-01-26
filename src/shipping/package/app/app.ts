97ccabc1d69619208a661e8018490b16fa7dbaa0
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2025-01-26 22:33:34 +0300

    Update app.ts - changing imports.

--- src/shipping/package/app/app.ts
+++ src/shipping/package/app/app.ts
@@ -5,8 +5,7 @@
 
 import Koa from 'koa';
 import bodyParser from "koa-bodyparser";
-
-var compress = require('koa-compress');
+import compress from 'koa-compress';
 
 import { apiRouter, healthzRouter, swaggerRouter } from './routes';
 import { logger, ILogger } from './util/logging'
