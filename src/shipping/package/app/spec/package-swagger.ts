d3c64c592a1c252dbd594160a926b37c88a3e0c5
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2025-01-28 12:57:38 +0300

    Update package-swagger.ts

--- src/shipping/package/app/spec/package-swagger.ts
+++ src/shipping/package/app/spec/package-swagger.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-//import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 import swaggerJSDoc from 'swagger-jsdoc';
 
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const options = {
   definition: {
